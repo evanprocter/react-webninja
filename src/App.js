497eea7e61af345f62343f1d326391d592581386
Evan Procter <evanprocter@gmail.com>
2018-12-14 08:06:57 -0500

    add NavBar component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+       
       </div>
     );
   }
 }
+
+
 
 export default App;
